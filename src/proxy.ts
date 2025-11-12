72e1db3a99a03b5a8f057336bc5a2e972103d5f1
June <smithjun@oregonstate.edu>
2025-11-11 20:18:35 -0800

    Middleware to proxy

---
+++ src/proxy.ts
@@ -0,0 +1,29 @@
+// import { type NextRequest } from 'next/server'
+// import { updateSession } from '@/utils/supabase/middleware'
+
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request)
+// }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      * Feel free to modify this pattern to include more paths.
+//      */
+//     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+//   ],
+// }
+import { NextResponse, NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+export const config = {
+  matcher: '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+}
